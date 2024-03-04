5a1c03a6b0bca88417e02cdd777307781f30b6db
Ekaterina_Bulgakova <carefulka@gmail.com>
2024-03-04 08:12:07 +0300

    Change DecorationDrawing.jsx: svg replaced

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -6,12 +6,12 @@ export function Header() {
     <div className="relative flex flex-col sm:pr-8">
       <DecorationDrawing
         direction="left"
-        className="absolute -left-[40px] bottom-0 hidden text-orange xs:block"
+        className="absolute -left-[32px] bottom-0 hidden text-orange xs:block"
       />
       <h1 className="m-0 cursor-default font-display text-xl lowercase text-orange">
         todos
       </h1>
-      <DecorationDrawing className="absolute -right-[40px] -top-[8px] hidden text-orange xs:block sm:-right-[8px]" />
+      <DecorationDrawing className="absolute -right-[32px] -top-[10px] hidden text-orange xs:block sm:-right-[8px]" />
       <ArrowDrawing className="absolute -right-[32px] bottom-0 hidden text-orange sm:block" />
       <p className="self-end text-base-bold sm:pr-5">
         Organize <span className="font-display text-orange">Your</span> Tasks
